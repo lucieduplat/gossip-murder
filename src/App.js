0cbc06c5d215a8293161d489bc882094185526aa
veensy <veensy@gmail.com>
2018-10-30 12:51:21 +0100

    dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import egnime_1 from './components/RUE DES MARMOUSETS/Egnime_1'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <egnime_1 />
+        
       </div>
     );
   }
---
+++ src/components/RUE DES MARMOUSETS/Egnime_1.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Egnime_1 extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Egnime_1
