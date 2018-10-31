7011359772f3a6245b50dff81afe7e0789a00510
julielm77 <julielyminh@gmail.com>
2018-10-31 14:24:14 +0100

    accueil

---
+++ src/components/accueil/Accueil.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Popup from './Popup.js';
+import './Accueil.css';
+import sang from './sang.png';
+import citrouille from './citrouille.png';
+import audio from './horreur.mp3' 
+
+class Accueil extends Component {
+  render() {
+    return (
+      <div className="popcenter">
+        <div className="sangcss">
+          <img src={sang} classeName="sangcss" alt="sang" />
+          <img src={sang} classeName="sangcss" alt="sang" />
+          <img src={sang} classeName="sang3css" alt="sang" />
+
+          <p className="flash">GOSSIP MURDER</p>
+
+          <img src={citrouille} classeName="citr" alt="citrouille" />
+
+          <br />
+          <br />
+          <br />
+          <audio id="audioPlayer" src={audio} autoPlay loop/> 
+          <Popup />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Accueil;
